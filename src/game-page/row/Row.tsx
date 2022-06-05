6b151552e5bc230e070a54d1b71a0721ed5af518
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-06-05 23:05:06 +1000

    Adding styles

--- src/game-page/row/Row.tsx
+++ src/game-page/row/Row.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import {RowState, TileState} from "../Models"
 import Tile from "../tile/Tile"
+import styles from "./Row.module.scss"
 
 const Row = (props: {rowState: RowState, onTileClick: (tile: TileState) => void}) =>
-    <div>
+    <div className={styles.row}>
         {
             props.rowState.tiles.map(
                 (tileState, index) => <Tile key={index} tileState={tileState} onTileClick={props.onTileClick}/>
